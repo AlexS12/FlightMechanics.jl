720eacdec766043d8e8677933ea81f8ad164fb22
Álex Sáez <alex.saez.12@gmail.com>
2018-08-16 19:35:41 +0200

    TEST: llh <->ecef using bowring1976 paper data

--- test/coordinates.jl
+++ test/coordinates.jl
@@ -37,3 +37,27 @@ ones_ = [1.0, 1.0, 1.0]
 @test ones_ ≈ body2wind(ones_..., 0., 0.)
 @test ones_ ≈ body2wind(0, 1, 2*0.70710678118654757, 45*pi/180., 0.)
 @test ones_ ≈ body2wind(0, 2*0.70710678118654757, 1, 0., 45*pi/180.)
+
+# llh ECEF (using data from
+# .. [1] Bowring, B. R. (1976). Transformation from spatial to geographical 
+# coordinates. Survey review, 23(181), 323-327.)
+import FlightMechanics: ecef2llh, llh2ecef
+import FlightMechanics.EarthConstants: Ellipsoid
+
+# From [1] Example 1 (page 325)
+x = 4114496.258  # m
+y = 0.0          # m
+z = 4870157.031  # m
+# Ellipsoid does not match any known standard
+a_bowring1976 = 6378249.145  # m
+b_bowring1976 = 6356514.870  # m
+finv_bowring1976 = a_bowring1976 / (a_bowring1976 - b_bowring1976)
+Bowring1976 = Ellipsoid(a_bowring1976, finv_bowring1976)
+
+exp_llh = [deg2rad(50.0), 0, 10000.0]
+llh = ecef2llh(x, y, z; ellipsoid=Bowring1976)
+@test isapprox(llh, exp_llh)
+
+exp_xyz = [4114496.258, 0.0, 4870157.031]  # m
+xyz = llh2ecef(deg2rad(50.0), 0, 10000.0; ellipsoid=Bowring1976)
+@test isapprox(xyz, exp_xyz)
