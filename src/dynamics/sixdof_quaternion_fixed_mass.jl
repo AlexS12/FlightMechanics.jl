3ce552ff64cc81557ef806581bd6cc19e9233d2b
Álex Sáez <alex.saez.12@gmail.com>
2018-09-21 23:12:36 +0200

    DOC: added documentation to six_dof_quaternion_fixed_mass

--- src/dynamics/sixdof_quaternion_fixed_mass.jl
+++ src/dynamics/sixdof_quaternion_fixed_mass.jl
@@ -1,4 +1,51 @@
+"""
+    six_dof_quaternion_fixed_mass(state, mass, inertia, forces, moments; k=0.0)
 
+Six degrees of freedom dynamic system using quaternions for attitude 
+representation and assuming fixed mass. 
+
+Flat Earth hypothesis is applied and Earth reference frame is considered
+inertial.
+
+It is considered that the aircraft xb-zb plane is a plane of symmetry so that
+Jxy and Jyz cross-product of inertia are zero and will not be taken into 
+account.
+
+# Arguments
+- `state::12-element Array{Number,1}`: state vector.
+    u, v, w: inertial linear velocity expressed in body axis. (m/s)
+    p, q, r: inertial rotatinal velocity expressed in body axis. (rad/s)
+    q0, q1, q2, q3: attitude given by quaternions.
+    xe, ye, ze: position wrt the inertial system origin expressed in Earth Axis. (m)
+- `mass::Number`: total mass of the aircraft (kg)
+- `inertia::3×3 Array{Number,2}`: inertia tensor (kg·m²)
+- `forces::3-element Array{Number,1}`: total forces expressed in body axis. (N)
+- `moments`::3-element Array{Number,1}: total moments expressed in body axis.(N·m)
+- `k`::Number: orthonormality error factor. 
+
+# Returns
+- `state_dot`: state vector derivative according to the equation of motion,
+    inertial properties and applied forces and moments.
+
+# Notes
+- See [1] (page 41, formula 1.4-23) or [2] (page 372, formulas 10-11 to 10-14)
+  for more information on quaternions <-> Euler angles conversions.
+- Orthonormality error factor is related to a numerical stability artifact used
+  in angular kinematic equations. Let λ = k * (1 - q0² - q1² - q2² - q3²) be
+  the orthonormality error. The term k·λ·q is added to the angular kinematic
+  equations in order to reduce the numerical integration error. According to
+  reference [2] k·Δt ≤ 1. See [2] (page 372) for more information on 
+  orthonormality error factor.
+
+# References
+.. [1] Stevens, B. L., Lewis, F. L., (1992). Aircraft control and simulation:
+ dynamics, controls design, and autonomous systems. John Wiley & Sons.
+ (Section 1.5, equations 1.5-4, page 46)
+
+.. [2] Zipfel, P. H. (2007). Modeling and simulation of aerospace vehicle
+ dynamics. American Institute of Aeronautics and Astronautics.
+ (page 368, figure 10.2)
+"""
 
 function six_dof_quaternion_fixed_mass(state, mass, inertia, forces, moments; k=0.0)
 
@@ -16,7 +63,8 @@ function six_dof_quaternion_fixed_mass(state, mass, inertia, forces, moments; k=
     q02, q12, q22, q32 = q0*q0, q1*q1, q2*q2, q3*q3
     
     # Normalization λ*t < 1 (See Zipfel Chapter 4.3.3.4 p.126)
-    # Zipfel, P. H. (2007). Modeling and simulation of aerospace vehicle dynamics. American Institute of Aeronautics and Astronautics.
+    # Zipfel, P. H. (2007). Modeling and simulation of aerospace vehicle dynamics.
+    # American Institute of Aeronautics and Astronautics.
     λ = k * (1.0 - q02 - q12 - q22 - q32)
 
     # Linear momentum equations
