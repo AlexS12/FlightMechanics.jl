f21891fb614b55a276b7c48f0c0d505986dbfe23
Álex Sáez <alex.saez.12@gmail.com>
2018-09-22 12:41:17 +0200

    BUG: dynamic models were not included in FlightMechanics

--- src/FlightMechanics.jl
+++ src/FlightMechanics.jl
@@ -5,4 +5,9 @@ include("atmosphere.jl")
 include("coordinates.jl")
 include("anemometry.jl")
 
+# 6 DOF dynamic models
+include("dynamics/sixdof_euler_fixed_mass.jl")
+include("dynamics/sixdof_quaternion_fixed_mass.jl")
+include("dynamics/sixdof_ecef_quaternion_fixed_mass.jl")
+
 end # module
