d4149ee15b2ad5f942fa4d453204c1bfd9b268b5
Álex Sáez <alex.saez.12@gmail.com>
2018-11-04 13:43:57 +0100

    ENH: added struct PointForcesMoments

---
+++ src/simulator/models/point_forces_moments.jl
@@ -0,0 +1,30 @@
+import Base: +
+
+export PointForcesMoments, translate_forces_moments, +
+
+
+# TODO: provide outer constructor checking dimensions?
+# TODO: think about associating it to a coordinate system
+struct PointForcesMoments
+    point::Array{Number, 1}
+    forces::Array{Number, 1}
+    moments::Array{Number, 1}
+end
+
+function translate_forces_moments(pfm::PointForcesMoments, point::Array{T, 1}) where T<:Number
+    r = pfm.point - point
+    f = pfm.forces
+    m = pfm.moments + cross(r, f)
+    PointForcesMoments(point, f, m)
+end
+
+function +(pfm1::PointForcesMoments, pfm2::PointForcesMoments, point::Array{T, 1}) where T<:Number
+
+    pfm1 = translate_forces_moments(pfm1, point)
+    pfm2 = translate_forces_moments(pfm2, point)
+    PointForcesMoments(point, pfm1.forces+pfm2.forces, pfm1.moments+pfm2.moments)
+end
+
+function +(pfm1::PointForcesMoments, pfm2::PointForcesMoments)
+    +(pfm1, pfm2, pfm1.point)
+end
