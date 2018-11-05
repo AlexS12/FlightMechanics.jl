0f6c0ced5204122de6c6b3ae769fc617f7f31690
Álex Sáez <alex.saez.12@gmail.com>
2018-11-05 21:37:46 +0100

    ENH: created ac abstract type

--- src/simulator/models/aircraft.jl
+++ src/simulator/models/aircraft.jl
@@ -3,12 +3,4 @@ export Aerodynamics, Aircraft
 
 abstract type Aerodynamics end
 
-
-struct Aircraft
-    mass_props::RigidSolid
-    pfm::PointForcesMoments
-
-    aerodynamics::Aerodynamics
-    propulsion::Propulsion
-    fcs::FCS
-end
+abstract type Aircraft end
