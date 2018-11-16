ef3dc7adec6ad9da15813ed0e89d6978a60760ff
Álex Sáez <alex.saez.12@gmail.com>
2018-11-16 22:55:13 +0100

    ENH: added getters

--- src/simulator/models/aircraft.jl
+++ src/simulator/models/aircraft.jl
@@ -4,3 +4,9 @@ export Aerodynamics, Aircraft
 abstract type Aerodynamics end
 
 abstract type Aircraft end
+
+
+get_mass_props(ac::Aircraft) = ac.mass_props
+get_pfm(ac::Aircraft) = ac.get_mass_props
+get_aerodynamics(ac::Aircraft) = ac.aerodynamics
+get_propulsion(ac::Aircraft) = ac.propulsion
