4a7c0795f3ae2380c2d55abcd59bbf590f02d90f
Álex Sáez <alex.saez.12@gmail.com>
2018-11-17 00:32:29 +0100

    ENH: propulsion calculation

--- src/simulator/aircrafts/C310/propulsion.jl
+++ src/simulator/aircrafts/C310/propulsion.jl
@@ -23,6 +23,13 @@ struct C310EngineRight<:C310Engine
     tanks::Array{RigidSolid, 1}
 end
 
+#Getters
+get_pfm(eng::C310Engine) = eng.pfm
+get_cj(eng::C310Engine) = eng.cj
+get_power(eng::C310Engine) = eng.power
+get_efficiency(eng::C310Engine) = eng.efficiency
+get_tanks(eng::C310Engine) = eng.tanks
+
 
 get_engine_position(prop::C310EngineLeft) = [-27.5, -70, 15.5] .* IN2M
 get_engine_position(prop::C310EngineRight) = [-27.5, 70, 15.5] .* IN2M
@@ -40,7 +47,7 @@ function get_default_fuel_tanks(prop::C310EngineRight)
 end
 
 function calculate_engine_power(eng::C310Engine, fcs::FCS,
-                                aerost::AeroState, state::State)
+                                aerostate::AeroState, state::State)
    # Max power at sea level
    Pmax0 = 260 * HP2WAT
    # Power assumed proportional to thrust lever
@@ -48,17 +55,37 @@ function calculate_engine_power(eng::C310Engine, fcs::FCS,
    # Assume supercharged engine, no variation of power with altitude until crtic altitude
    Pm = P0 * 1.0
    # Calculate fuel consumption also
-   return Pm, 0
+   cj = 0.0
+   return Pm, cj
 end
 
 function calculate_propeller_thrust(eng::C310Engine, fcs::FCS,
-                                    aerost::AeroState, state::State, Pm)
+                                    aerostate::AeroState, state::State, Pm)
    # Assume constant propulsive efficiency
    ηp = 0.75
    # Calculate thrust
    thrust = ηp * Pm / aero.tas
-   return thrust
+   return thrust, ηp
 end
+
+
+function calculate(eng::C310Engine, fcs::FCS, aerostate::AeroState, state::State;
+                   consume_fuel=false)
+    Pm, cj = calculate_engine_power(eng, fcs, aeroste)
+    thrust, ηp = calculate_propeller_thrust(eng, fcs, aerostate, state)
+
+    if consume_fuel==true
+        error("fuel consumption not implemented")
+    else
+        tanks = get_tanks(eng)
+
+    # TODO: Torque calcualtion is missing
+    pfm = PointForcesMoments(get_engine_position(prop),
+                             [thrust, 0, 0],
+                             [0, 0, 0]
+                             )
+
+    return typeof(eng)(pfm, cj, power, ηp, tanks)
 
 
 get_thrust_setting(eng::C310EngineLeft, fcs::FCS) = get_value(fcs.t1)
--- src/simulator/models/propulsion.jl
+++ src/simulator/models/propulsion.jl
@@ -11,3 +11,36 @@ struct Propulsion
     efficiency::Number
     engines::Array{Engine, 1}
 end
+
+
+# Getters
+get_pfm(prop::Propulsion) = prop.pfm
+get_cj(prop::Propulsion) = prop.cj
+get_power(prop::Propulsion) = prop.power
+get_efficiency(prop::Propulsion) = prop.efficiency
+get_engines(prop::Propulsion) = prop.engines
+
+
+function get_propulsion_position(prop::Propulsion)
+    sum(get_engine_position(get_engines(prop))) / len(get_engines(prop))
+
+
+function calculate(prop::Propulsion, fcs::FCS, aerostate::AeroState, state::State;
+                   consume_fuel=false)
+
+    engines = get_engines(prop)
+
+    power = 0.
+    pfm = PointForcesMoments(get_propulsion_position(prop), [0, 0, 0], [0, 0, 0])
+    cj = 0
+    efficiency = 0
+
+    for (ii, eng)=engines
+        engines[ii] = calculate(eng, fcs, aerostate, state; consume_fuel)
+        # TODO: use get_engine_orientation and rotate every engine pfm to body
+        pfm += rotate(get_pfm(engines[ii]), get_engine_orientation(eng)...)
+        power += get_power(engines[ii])
+        # TODO: cj and efficiency
+
+    Propulsion(pfm, cj, power, efficiency, engines)
+end
