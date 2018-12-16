66ab5905d069e211fba914d7cdd8449614770a2f
Álex Sáez <alex.saez.12@gmail.com>
2018-12-16 12:46:26 +0100

    DOC: minor documentation on Environment

--- src/simulator/models/environment.jl
+++ src/simulator/models/environment.jl
@@ -11,8 +11,40 @@ export Environment, DefaultEnvironment, calculate_environment,
 
 
 # -------- ATMOSPHERE --------
+"""
+    Atmosphere
+
+Atmospheric information at a given altitude.
+"""
 abstract type Atmosphere end
 
+
+"""
+    AtmosphereISA(temperature, pressure, density, sound_velocity)
+
+International Standard Atmosphere 1976.
+
+# See also
+
+    atmosphere_isa(height)
+
+# References
+
+- [1] U.S. Standard Atmosphere, 1976, U.S. Government Printing Office,
+        Washington, D.C., 1976
+
+From: https://en.wikipedia.org/wiki/U.S._Standard_Atmosphere
+
+| Layer | h (m) | p (Pa)  | T (K)  | ``α`` (K/m) |
+|-------|-------|---------|--------|-------------|
+| 0     | 0     | 101325  | 288.15 | -0.0065     |
+| 1     | 11000 | 22632.1 | 216.65 | 0           |
+| 2     | 20000 | 5474.89 | 216.65 | 0.001       |
+| 3     | 32000 | 868.019 | 228.65 | 0.0028      |
+| 4     | 47000 | 110.906 | 270.65 | 0           |
+| 5     | 51000 | 66.9389 | 270.65 | -0.0028     |
+| 6     | 71000 | 3.95642 | 214.65 | -0.002      |
+"""
 struct AtmosphereISA<:Atmosphere
     temperature::Number  # K
     pressure::Number  # Pa
@@ -33,8 +65,26 @@ function calculate_atmosphere(atmos::AtmosphereISA, state::State)
 end
 
 # -------- WIND --------
+"""
+    Wind
+
+Wind information at a given placement.
+"""
 abstract type Wind end
 
+
+"""
+    ConstantWind(direction, intensity, vertical)
+
+Constant wind structure:
+  - direction: wind blowing from that direction [rad]
+  - intensity: wind speed [m/s]
+  - vertical: vertical wind speed [m/s] positive blowing upwards.
+
+ # Constructors
+
+    ConstantWind(): No wind.
+"""
 struct ConstantWind<:Wind
     direction::Number  # coming from [rad]
     intensity::Number  # positive blowing to ac [m/s]
@@ -48,6 +98,12 @@ get_direction(wind::Wind) = wind.direction
 get_intensity(wind::Wind) = wind.intensity
 get_vertical(wind::Wind) = wind.vertical
 
+
+"""
+    get_wind_NED(wind::Wind)
+
+Express wind in local horizon axis [N, E, D].
+"""
 function get_wind_NED(wind::Wind)
     # coming from
     wind_N = wind.intensity * cos(wind.direction + π)
@@ -56,6 +112,11 @@ function get_wind_NED(wind::Wind)
     return [wind_N, wind_E, wind_D]
 end
 
+"""
+    get_wind_body(wind::Wind, state::State)
+
+Express wind body axis [N, E, D] given the `Attitude` stored in `State`.
+"""
 function get_wind_body(wind::Wind, state::State)
     wind_ned = get_wind_NED(wind)
     hor2body(wind_ned..., get_quaternion(state)...)
@@ -66,6 +127,16 @@ calculate_wind(wind::ConstantWind, state::State) = wind
 # -------- GRAVITY --------
 abstract type Gravity end
 
+"""
+    EarthConstantGravity(value, body_vector)
+
+Earth Constant gravity model with value 9.80665 m/s² and oriented towards
+local horizon Z axis.
+
+# Constructors
+    EarthConstantGravity(value, body_vector)
+    EarthConstantGravity()
+"""
 struct EarthConstantGravity<:Gravity
     value::Number  # m/s²
     body_vector::Array{T, 1} where T<:Number
@@ -88,6 +159,14 @@ function calculate_gravity(grav::EarthConstantGravity, state::State)
 end
 
 # -------- ENVRIONMENT --------
+"""
+    Environment(atmos::Atmosphere, wind::Wind, grav::Gravity)
+
+Environment information composed of atmosphere, wind and gravity.
+
+# Constructors
+    DefatultEnvironment(): ISA atomosphere, Constant zero wind, and Constant gravity.
+"""
 struct Environment
     atmos::Atmosphere
     wind::Wind
