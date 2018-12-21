33128bfb0a82cc7b475b5d33f7a3ec9e51d56ef2
Álex Sáez <alex.saez.12@gmail.com>
2018-12-21 17:52:29 +0100

    TEST: position

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,13 +5,15 @@ else
     using Test
 end
 
-# write your own tests here
+
 @testset "atmosphere" begin include("atmosphere.jl") end
 @testset "coordinates" begin include("coordinates.jl") end
 @testset "anemometry" begin include("anemometry.jl") end
 @testset "mechanics" begin include("mechanics.jl") end
+
 @testset "point forces moments" begin include("simulator/models/point_forces_moments.jl") end
 @testset "mass" begin include("simulator/models/mass.jl") end
 @testset "attitude" begin include("simulator/models/attitude.jl") end
+@testset "position" begin include("simulator/models/position.jl") end
 
 @testset "c310" begin include("c310.jl") end
