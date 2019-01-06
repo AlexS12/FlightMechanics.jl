dc172e352706583abb938b0a07bb137af5b10fcc
Álex Sáez <alex.saez.12@gmail.com>
2019-01-06 18:11:20 +0100

    ENH: added gyroscopic effects

--- src/dynamics/sixdof_quaternion_fixed_mass.jl
+++ src/dynamics/sixdof_quaternion_fixed_mass.jl
@@ -1,7 +1,8 @@
 export six_dof_quaternion_fixed_mass
 
 """
-    six_dof_quaternion_fixed_mass(state, mass, inertia, forces, moments; k=0.0)
+    six_dof_quaternion_fixed_mass(state, mass, inertia, forces, moments,
+                                  h=[0.0, 0.0, 0.0], k=0.0)
 
 Six degrees of freedom dynamic system using quaternions for attitude
 representation and assuming fixed mass.
@@ -13,6 +14,9 @@ It is considered that the aircraft xb-zb plane is a plane of symmetry so that
 Jxy and Jyz cross-product of inertia are zero and will not be taken into
 account.
 
+The effects of the angular momentum produced by spinning rotors is taken into
+account with the optional argument `h`.
+
 # Arguments
 - `state::12-element Array{Number,1}`: state vector.
     u, v, w: inertial linear velocity expressed in body axis. (m/s)
@@ -23,6 +27,8 @@ account.
 - `inertia::3×3 Array{Number,2}`: inertia tensor (kg·m²)
 - `forces::3-element Array{Number,1}`: total forces expressed in body axis. (N)
 - `moments::3-element Array{Number,1}`: total moments expressed in body axis.(N·m)
+- `h::3-element Array{Number,1}`: Additional angular momentum contributions such
+  as those coming from spinning rotors (kg·m²/s).
 - `k::Number`: orthonormality error factor.
 
 # Returns
@@ -48,13 +54,10 @@ account.
  dynamics. American Institute of Aeronautics and Astronautics.
  (page 368, figure 10.2)
 """
-function six_dof_quaternion_fixed_mass(state, mass, inertia, forces, moments; k=0.0)
+function six_dof_quaternion_fixed_mass(state, mass, inertia, forces, moments,
+                                       h=[0.0, 0.0, 0.0], k=0.0)
 
     m = mass
-    Ix = inertia[0, 0]
-    Iy = inertia[1, 1]
-    Iz = inertia[2, 2]
-    Jxz = -inertia[0, 2]
 
     u, v, w, p, q, r, q0, q1, q2, q3, xe, ye, ze = state
 
@@ -74,13 +77,35 @@ function six_dof_quaternion_fixed_mass(state, mass, inertia, forces, moments; k=
     w_dot = Fz / m + q * u - p * v
 
     # Angular momentum equations
+    Ix = inertia[1, 1]
+    Iy = inertia[2, 2]
+    Iz = inertia[3, 3]
+    Jxz = -inertia[1, 3]
+
+    Jxz2 = Jxz*Jxz
+    Γ = (Ix*Iz - Jxz2)
+    temp = (Ix + Iz - Iy)
     Jxz2 = Jxz*Jxz
     den = (Ix*Iz - Jxz2)
     temp = (Ix + Iz - Iy)
 
-    p_dot = (L*Iz + N*Jxz - q*r*(Iz*Iz - Iz*Iy + Jxz2) + p*q * Jxz * temp) / den
-    q_dot = (M + (Iz - Ix) * p*r - Jxz * (p*p - r*r)) / Iy
-    r_dot = (L*Jxz + N*Ix + p*q * (Ix*Ix - Ix*Iy + Jxz2) - q*r * Jxz * temp) / den
+    # Engine angular momentum contribution
+    hx, hy, hz = h
+
+    rhy_qhz = (r*hy - q*hz)
+    qhx_phy = (q*hx - p*hy)
+
+    pe_dot = Iz * rhy_qhz + Jxz * qhx_phy
+    qe_dot = -r*hx + p*hz
+    re_dot = Jxz * rhy_qhz + Ix * qhx_phy
+
+    # Angular momentum equations
+    p_dot = L*Iz + N*Jxz - q*r*(Iz*Iz - Iz*Iy + Jxz2) + p*q * Jxz * temp + pe_dot
+    p_dot /= Γ
+    q_dot = M + (Iz - Ix) * p*r - Jxz * (p*p - r*r) + qe_dot
+    q_dot /= Iy
+    r_dot = L*Jxz + N*Ix + p*q * (Ix*Ix - Ix*Iy + Jxz2) - q*r * Jxz * temp + re_dot
+    r_dot /= Γ
 
     # Angular Kinematic equations
     q0_dot = 0.5 * (     - p*q1 - q*q2 - r*q3 ) + λ * q0
@@ -93,6 +118,5 @@ function six_dof_quaternion_fixed_mass(state, mass, inertia, forces, moments; k=
     ye_dot = 2*u*(q1*q2 + q0*q3) + v*(q02 - q12 + q22 - q32) + 2*w*(q2*q3 - q0*q1)
     ze_dot = 2*u*(q1*q3 - q0*q2) + 2*v*(q2*q3 + q0*q1) + w*(q02 - q12 - q22 + q32)
 
-    return [u_dot v_dot w_dot p_dot q_dot r_dot q0_dot q1_dot q2_dot q3_dot xe_dot ye_dot ze_dot]
-
+    [u_dot v_dot w_dot p_dot q_dot r_dot q0_dot q1_dot q2_dot q3_dot xe_dot ye_dot ze_dot]
 end
