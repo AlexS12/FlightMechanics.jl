394a4d63fe81f27897aadb605470014b657aec3f
Álex Sáez <alex.saez.12@gmail.com>
2019-01-06 17:33:49 +0100

    DOC: added h to list of arguments. Minor STY changes

--- src/dynamics/sixdof_euler_fixed_mass.jl
+++ src/dynamics/sixdof_euler_fixed_mass.jl
@@ -2,7 +2,8 @@
 export six_dof_euler_fixed_mass
 
 """
-    six_dof_euler_fixed_mass(state, mass, inertia, forces, moments)
+    six_dof_euler_fixed_mass(state, mass, inertia, forces, moments,
+                             h=[0.0, 0.0, 0.0])
 
 Six degrees of freedom dynamic system using Euler angles for attitude
 representation and assuming fixed mass.
@@ -14,6 +15,9 @@ It is considered that the aircraft xb-zb plane is a plane of symmetry so that
 Jxy and Jyz cross-product of inertia are zero and will not be taken into
 account.
 
+The effects of the angular momentum produced by spinning rotors is taken into
+account with the optional argument `h`.
+
 # Arguments
 - `state::12-element Array{Number,1}`: state vector.
     u, v, w: inertial linear velocity expressed in body axis. (m/s)
@@ -24,6 +28,8 @@ account.
 - `inertia::3×3 Array{Number,2}`: inertia tensor (kg·m²)
 - `forces::3-element Array{Number,1}`: total forces expressed in body axis. (N)
 - `moments::3-element Array{Number,1}`: total moments expressed in body axis.(N·m)
+- `h::3-element Array{Number,1}`: Additional angular momentum contributions such
+  as those coming from spinning rotors (kg·m²/s).
 
 # Returns
 - `state_dot`: state vector derivative according to the equation of motion,
@@ -43,7 +49,7 @@ account.
  kinematic equations)
 """
 function six_dof_euler_fixed_mass(state, mass, inertia, forces, moments,
-                                  eng_h=[0.0, 0.0, 0.0])
+                                  h=[0.0, 0.0, 0.0])
 
     m = mass
     u, v, w, p, q, r, ψ, θ, ϕ, xe, ye, ze = state
@@ -71,7 +77,7 @@ function six_dof_euler_fixed_mass(state, mass, inertia, forces, moments,
     temp = (Ix + Iz - Iy)
 
     # Engine angular momentum contribution
-    hx, hy, hz = eng_h
+    hx, hy, hz = h
 
     rhy_qhz = (r*hy - q*hz)
     qhx_phy = (q*hx - p*hy)
@@ -83,10 +89,8 @@ function six_dof_euler_fixed_mass(state, mass, inertia, forces, moments,
     # Angular momentum equations
     p_dot = L*Iz + N*Jxz - q*r*(Iz*Iz - Iz*Iy + Jxz2) + p*q * Jxz * temp + pe_dot
     p_dot /= Γ
-
     q_dot = M + (Iz - Ix) * p*r - Jxz * (p*p - r*r) + qe_dot
     q_dot /= Iy
-
     r_dot = L*Jxz + N*Ix + p*q * (Ix*Ix - Ix*Iy + Jxz2) - q*r * Jxz * temp + re_dot
     r_dot /= Γ
 
@@ -101,6 +105,5 @@ function six_dof_euler_fixed_mass(state, mass, inertia, forces, moments,
     ye_dot =  cθ*sψ * u + (sϕ*sθ*sψ + cϕ*cψ) * v + (cϕ*sθ*sψ - sϕ*cψ) * w
     ze_dot = -sθ    * u +  sϕ*cθ             * v +  cϕ*cθ             * w
 
-    return [u_dot v_dot w_dot p_dot q_dot r_dot ψ_dot θ_dot ϕ_dot xe_dot ye_dot ze_dot]
-
+    [u_dot v_dot w_dot p_dot q_dot r_dot ψ_dot θ_dot ϕ_dot xe_dot ye_dot ze_dot]
 end
