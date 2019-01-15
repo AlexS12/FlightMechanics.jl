10b362d6998be8dc1c2102516465f7a0432ac1d0
Álex Sáez <alex.saez.12@gmail.com>
2019-01-15 20:51:16 +0100

    BUG: wind is always interpreted as coming from (NED, body...)

--- src/simulator/models/wind.jl
+++ src/simulator/models/wind.jl
@@ -13,20 +13,40 @@ Wind information at a given placement.
 """
 abstract type Wind end
 
+"""
+    get_wind_dir_int_ver(wind::Wind)
+
+- direction: wind blowing from that direction [rad]
+- intensity: wind speed [m/s]
+- vertical: vertical wind speed [m/s] positive blowing upwards.
+"""
 get_wind_dir_int_ver(wind::Wind) = [wind.direction, wind.intensity, wind.vertical]
+
+"""
+Wind blowing from this direction [rad]
+"""
 get_direction(wind::Wind) = wind.direction
+
+"""
+Wind magnitude ≥ 0  [m/s]
+"""
 get_intensity(wind::Wind) = wind.intensity
+
+"""
+Wind vertical magnitude [m/s]. Possitive blowing up.
+"""
 get_vertical(wind::Wind) = wind.vertical
 
 """
     get_wind_NED(wind::Wind)
 
-Express wind in local horizon axis [N, E, D].
+Express wind in local horizon axis [N, E, D]. Must be interpreted as wind coming
+from north, east and west [m/s].
 """
 function get_wind_NED(wind::Wind)
     # coming from
-    wind_N = wind.intensity * cos(wind.direction + π)
-    wind_E = wind.intensity * sin(wind.direction + π)
+    wind_N = wind.intensity * cos(wind.direction)
+    wind_E = wind.intensity * sin(wind.direction)
     wind_D = wind.vertical
     return [wind_N, wind_E, wind_D]
 end
@@ -34,7 +54,8 @@ end
 """
     get_wind_body(wind::Wind, att::Attitude)
 
-Express wind body axis [N, E, D] given the `Attitude`.
+Express wind in body axis given the `Attitude`. Must be interpreted as wind coming
+from forward, right and down directions [m/s]
 """
 function get_wind_body(wind::Wind, att::Attitude)
     wind_ned = get_wind_NED(wind)
