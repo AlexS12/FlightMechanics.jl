2b5587ef2b12cd896ca30cd1e45a17ac900531f9
Parth viradiya <36721553+parthp08@users.noreply.github.com>
2019-06-15 17:32:58 +0530

    corrected one bug

--- src/models/aircraft.jl
+++ src/models/aircraft.jl
@@ -9,7 +9,7 @@ abstract type Aircraft end
 
 
 get_mass_props(ac::Aircraft) = ac.mass_props
-get_pfm(ac::Aircraft) = ac.get_mass_props
+get_pfm(ac::Aircraft) = ac.pfm
 get_aerodynamics(ac::Aircraft) = ac.aerodynamics
 get_propulsion(ac::Aircraft) = ac.propulsion
 
