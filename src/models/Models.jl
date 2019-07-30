20554d2ea2fec01bc67dab8266288fd6e82e5cc7
Álex Sáez <alex.saez.12@gmail.com>
2019-07-30 22:55:47 +0200

    REF: exporting state methods from models

--- src/models/Models.jl
+++ src/models/Models.jl
@@ -7,12 +7,20 @@ export
 Attitude, get_euler_angles, get_quaternions,
 # Position
 Position, PositionLLH, PositionEarth, PositionECEF,
-get_position, get_llh, get_xyz_earth, get_xyz_ecef, get_height
+get_position, get_llh, get_xyz_earth, get_xyz_ecef, get_height,
+# State
+State,
+get_attitude,
+get_body_velocity, get_horizon_velocity, get_flight_path_angle,
+get_body_ang_velocity, get_turn_rate,
+get_body_accel, get_horizon_accel,
+get_body_ang_accel,
+get_sixdof_euler_fixed_mass_state
 
 
 include("attitude.jl")
 include("position.jl")
-    include("state.jl")
+include("state.jl")
     include("mass.jl")
     include("point_forces_moments.jl")
     include("atmosphere.jl")
