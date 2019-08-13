2533b77cc3cdd171ae97af4844dafbcebcc7607b
Álex Sáez <alex.saez.12@gmail.com>
2019-08-13 20:41:56 +0200

    TEST: uvw_to_tas...

--- test/flight_mechanics.jl
+++ test/flight_mechanics.jl
@@ -15,3 +15,13 @@ pqr = euler_angles_rates_to_body_angular_velocity(ear..., θ, ϕ)
 pqr1 = euler_angles_rates_to_body_angular_velocity(ψ_dot, 0.0, 0.0, θ, ϕ)
 pqr2 = turn_rate_angular_velocity(ψ_dot, θ, ϕ)
 @test isapprox(pqr1, pqr2)
+
+# Test uvw_to_tasαβ
+exp_tas = 100  # m/s
+exp_α = 0.01  # rad
+exp_β = 0.005  # rad
+u, v, w = wind2body(tas, 0, 0, exp_α, exp_β)
+tas, α, β = uvw_to_tasαβ(u, v, w)
+@test isapprox(tas, exp_tas)
+@test isapprox(α, exp_α)
+@test isapprox(β, exp_β, rtol=1e-5)
