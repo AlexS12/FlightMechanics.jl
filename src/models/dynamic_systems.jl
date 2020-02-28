266d7c04bf2487259d95b746d3898bf608847c34
Álex Sáez <alex.saez.12@gmail.com>
2020-02-28 12:26:35 +0100

    ENH: added SixDOFQuaternionFixedMass system

--- src/models/dynamic_systems.jl
+++ src/models/dynamic_systems.jl
@@ -51,7 +51,43 @@ end
 
 # TODO: implementations for the rest of the systems
 # ---------------------- SixDOFQuaternionFixedMass ----------------------
+struct SixDOFQuaternionFixedMass <: DynamicSystem
+    x :: Array{T, 1} where T<: Number
+    state_equation :: Function
+    state_equation_ode_wrapper! :: Function
+    # TODO: must include k
+    # k :: Number  # orthonormality error factor (see six_dof_quaternion_fixed_mass)
+end
 
+function six_dof_quaternion_fixed_mass_ode_wrapper!(dx, x, p, t, k=0.0)
+    mass = p[1]
+    inertia = p[2]
+    forces = p[3]
+    moments = p[4]
+    h = p[5]
+    dx[:] = six_dof_quaternion_fixed_mass(x, mass, inertia, forces, moments, h, k)
+end
+
+SixDOFQuaternionFixedMass() = SixDOFQuaternionFixedMass(fill(NaN, 13), six_dof_quaternion_fixed_mass, six_dof_quaternion_fixed_mass_ode_wrapper!)
+SixDOFQuaternionFixedMass(x) = SixDOFQuaternionFixedMass(x, six_dof_quaternion_fixed_mass, six_dof_quaternion_fixed_mass_ode_wrapper!)
+
+function convert(::Type{State}, ds::SixDOFQuaternionFixedMass)
+    # TODO: add x_dot to Dynamic system to fill accel and ang_accel
+    x = get_x(ds)
+    # TODO: only PositionEarth is being updated. What about LLH or ECEF...
+    State(PositionEarth(x[11:13]...), Attitude(x[7:10]...), x[1:3], x[4:6], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0])
+end
+
+function convert(::Type{SixDOFQuaternionFixedMass}, state::State)
+    x = [
+            get_body_velocity(state)...,
+            get_body_ang_velocity(state)...,
+            get_quaternions(state)...,
+            get_xyz_earth(state)...,
+        ]
+
+    SixDOFQuaternionFixedMass(x)
+end
 # ---------------------- SixDOFECEFQuaternionFixedMass ----------------------
 
 # ---------------------- SixDOFAeroEulerFixedMass ----------------------
