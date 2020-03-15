d5b66ba5d61e6c4d5c66916104f407136b928dfc
asaez <alex.saez.12@gmail.com>
2020-03-15 17:38:28 +0100

    ENH: aerostate does not assume ISA atmosphere anymore

--- test/simulator/models/aero_state.jl
+++ test/simulator/models/aero_state.jl
@@ -19,10 +19,16 @@ wind = ConstantWind(0.0, 10.0, 0.0)
 grav = EarthConstantGravity()
 env = Environment(atm, wind, grav)
 
+env = calculate_environment(env, get_position(state))
+
 # AEROSTATE
 tas = 110
 alpha, beta = 0.0, 0.0
-aerost = AeroState(tas, alpha, beta, 0.0)
+p = get_pressure(env)
+ρ = get_density(env)
+a = get_sound_velocity(env)
+
+aerost = AeroState(tas, alpha, beta, p, ρ, a)
 
 @test isequal(check_state_aerostate_env_coherence(state, aerost, env), true)
 
@@ -36,7 +42,7 @@ state, aerost = generate_state_aerostate(pos, att, tas, alpha, beta, env,
 # AEROSTATE
 tas = 90
 alpha, beta = 0.0, 0.0
-aerost_exp = AeroState(tas, alpha, beta, 0.0)
+aerost_exp = AeroState(tas, alpha, beta, p, ρ, a)
 
 att = Attitude(pi, 0.0, 0.0)
 # TEST generate_state_aerostate
@@ -50,7 +56,7 @@ state, aerost = generate_state_aerostate(pos, att, tas, alpha, beta, env, ang_ve
 # AEROSTATE
 tas = 90
 alpha, beta = 0.0, 0.0
-aerost_exp = AeroState(tas, alpha, beta, 0.0)
+aerost_exp = AeroState(tas, alpha, beta, p, ρ, a)
 
 att = Attitude(pi, 0.0, 0.0)
 # TEST generate_state_aerostate
@@ -65,3 +71,36 @@ state_wrong = State(get_position(state),
                     )
 @test isequal(check_state_aerostate_env_coherence(state_wrong, aerost, env),
               false)
+
+# TODO: look for a name for this test
+pos = PositionEarth(0.0, 0.0, -3000.0)
+atm = AtmosphereISA(pos)
+wind = ConstantWind(0.0, 0.0, 0.0)
+grav = EarthConstantGravity()
+env = Environment(atm, wind, grav)
+
+tas = 100.0 # m/s
+α = 4.0 * DEG2RAD
+β = 0.0  # rad
+
+att = Attitude(0.0, 4.0 * DEG2RAD, 0.0)
+vel = wind2body(tas, 0, 0, α, β)
+ang_vel = [0.0, 0.0, 0.0]
+accel = [0.0, 0.0, 0.0]
+ang_accel = [0.0, 0.0, 0.0]
+
+env = calculate_environment(env, pos)
+state1 = State(pos, att, vel, ang_vel, accel, ang_accel)
+aerostate1 = AeroState(state1, env)
+
+state2, aerostate2 = generate_state_aerostate(
+    pos,
+    att,
+    tas,
+    α,
+    β,
+    env,
+    ang_vel,
+    accel,
+    ang_accel,
+)
