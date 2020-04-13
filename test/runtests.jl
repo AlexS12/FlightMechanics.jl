092445f0b3110d4c165b48bb98acfe3693d85916
asaez <alex.saez.12@gmail.com>
2020-04-13 17:52:23 +0200

    REF: removed simulator folder from tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,13 +8,13 @@ using FlightMechanics
 @testset "mechanics" begin include("mechanics.jl") end
 @testset "flight mechanics" begin include("flight_mechanics.jl") end
 
-@testset "pfm" begin include("simulator/models/point_forces_moments.jl") end
-@testset "mass" begin include("simulator/models/mass.jl") end
-@testset "attitude" begin include("simulator/models/attitude.jl") end
-@testset "position" begin include("simulator/models/position.jl") end
-@testset "aerodynamics" begin include("simulator/models/aerodynamics.jl") end
-@testset "aerostate" begin include("simulator/models/aero_state.jl") end
-@testset "trimmer" begin include("simulator/models/trimmer.jl") end
+@testset "pfm" begin include("models/point_forces_moments.jl") end
+@testset "mass" begin include("models/mass.jl") end
+@testset "attitude" begin include("models/attitude.jl") end
+@testset "position" begin include("models/position.jl") end
+@testset "aerodynamics" begin include("models/aerodynamics.jl") end
+@testset "aerostate" begin include("models/aero_state.jl") end
+@testset "trimmer" begin include("models/trimmer.jl") end
 
-@testset "ac: c310" begin include("simulator/aircrafts/c310.jl") end
-@testset "ac: f16" begin include("simulator/aircrafts/f16.jl") end
+@testset "ac: c310" begin include("aircrafts/c310.jl") end
+@testset "ac: f16" begin include("aircrafts/f16.jl") end
