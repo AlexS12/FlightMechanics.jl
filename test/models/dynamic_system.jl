b71fb3380e401f80785908056a2242adaa8d8e8e
asaez <alex.saez.12@gmail.com>
2020-04-19 10:41:01 +0200

    TEST: checking convert methods between state and dynamic_system

--- test/models/dynamic_system.jl
+++ test/models/dynamic_system.jl
@@ -1,7 +1,22 @@
-@testset "SixDOFEulerFixedMass conversion" begin
+using FlightMechanics.Models
 
+
+pos = PositionEarth(1000.0, 500.0, 1000.0)
+att = Attitude(π/4, π/32, π/8)
+vel = [100.0, 0.0, 0.0]
+ang_vel = [π/90, π/18, π/9]
+accel = [1.5, 0.3, 0.5]
+ang_accel = [π/45, π/60, π/30]
+state = State(pos, att, vel, ang_vel, accel, ang_accel)
+
+@testset "SixDOFEulerFixedMass conversion" begin
+    x = convert(SixDOFEulerFixedMass, state)
+    state1 = convert(State, x)
+    @test isapprox(state, state1)
 end
 
 @testset "SixDOFQuaternionFixedMass" begin
-
+    x = convert(SixDOFQuaternionFixedMass, state)
+    state2 = convert(State, x)
+    @test isapprox(state, state2)
 end
