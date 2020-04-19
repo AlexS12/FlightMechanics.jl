eb8a7ffa98818ec1a428f33a7f5b99ed08127a8a
asaez <alex.saez.12@gmail.com>
2020-04-19 19:25:45 +0200

    ENH: Added 3 new structs to store positions

--- src/models/position.jl
+++ src/models/position.jl
@@ -1,3 +1,6 @@
+using FlightMechanics
+
+
 """
     Position(llh, xzy_earth, xyz_ecef)
 
@@ -41,4 +44,135 @@ function isapprox(x::Position, y::Position; rtol=1e-8, atol=0.0, nans=false)
     ])
 
     return result
+end
+
+# -------------- POSITIONS --------------
+struct ECEFPosition
+    x :: Number  # m
+    y :: Number  # m
+    z :: Number  # m
+end
+
+
+struct LLHPosition
+    lat :: Number  # rad
+    lon :: Number  # rad
+    height :: Number  # m
+    ellipsoid :: Ellipsoid
+end
+
+LLHPosition(ellipsoid) = LLHPosition(NaN, NaN, NaN, ellipsoid)
+LLHPosition(lat, lon, h) = LLHPosition(lat, lon, h, WGS84)
+LLHPosition() = LLHPosition(NaN, NaN, NaN)
+
+
+struct EarthPosition
+    x :: Number  # m
+    y :: Number  # m
+    z :: Number  # m
+    ref_point :: T where T<:Union{LLHPosition}
+end
+
+EarthPosition(x, y, z) = EarthPosition(x, y, z, LLHPosition(0, 0, 0))
+EarthPosition(ref_point) = EarthPosition(NaN, NaN, NaN, ref_point)
+
+
+# -------------- CONVERTERS --------------
+convert(llh::LLHPosition, pos::ECEFPosition) = LLHPosition(
+    ecef2llh(
+        pos.x,
+        pos.y,
+        pos.z,
+        ellipsoid=llh.ellipsoid,
+    )...
+)
+
+
+convert(::Type{ECEFPosition}, pos::LLHPosition) = ECEFPosition(
+    llh2ecef(
+        pos.lat,
+        pos.lon,
+        pos.height,
+        ellipsoid=pos.ellipsoid
+    )...
+)
+
+
+function convert(earth::EarthPosition, ecef::ECEFPosition)
+    ref_point_ecef = convert(ECEFPosition, earth.ref_point)
+
+    dx = ecef.x - ref_point_ecef.x
+    dy = ecef.y - ref_point_ecef.y
+    dz = ecef.z - ref_point_ecef.z
+
+    dx, dy, dz = ecef2hor(dx, dy, dz, earth.ref_point.lat, earth.ref_point.lon)
+
+    return EarthPosition(dx, dy, dz, earth.ref_point)
+end
+
+
+function convert(::Type{ECEFPosition}, earth::EarthPosition)
+    ref_point_ecef = convert(ECEFPosition, earth.ref_point)
+
+    dx, dy, dz = hor2ecef(
+        earth.x,
+        earth.y,
+        earth.z,
+        earth.ref_point.lat,
+        earth.ref_point.lon
+    )
+
+    return ECEFPosition(
+        ref_point_ecef.x + dx,
+        ref_point_ecef.y + dy,
+        ref_point_ecef.z + dz
+    )
+end
+
+
+function convert(::Type{LLHPosition}, earth::EarthPosition)
+    ecef_pos = convert(ECEFPosition, earth)
+    return convert(earth.ref_point, ecef_pos)
+end
+
+
+function convert(llh_to::LLHPosition, llh_from::LLHPosition)
+    ecef_from = convert(ECEFPosition, llh_from)
+    llh_to = convert(llh_to, ecef_from)
+    return llh_to
+end
+
+
+function convert(earth::EarthPosition, llh::LLHPosition)
+    ecef = convert(ECEFPosition, llh)
+    earth = convert(earth, ecef)
+    return earth
+end
+
+
+# -------------- COMPARISON --------------
+function isapprox(x::ECEFPosition, y::ECEFPosition; rtol=1e-8, atol=0.0, nans=false)
+    return isapprox([x.x, x.y, x.z], [y.x, y.y, y.z]; rtol=rtol, atol=atol, nans=nans)
+end
+
+
+function isapprox(x::LLHPosition, y::LLHPosition; rtol=1e-8, atol=0.0, nans=false)
+    r = all([
+        isapprox(x.lat, y.lat; rtol=rtol, atol=atol, nans=nans),
+        isapprox(x.lon, y.lon; rtol=rtol, atol=atol, nans=nans),
+        isapprox(x.height, y.height; rtol=rtol, atol=atol, nans=nans),
+        x.ellipsoid == y.ellipsoid,
+    ])
+    return r
+end
+
+
+function isapprox(x::EarthPosition, y::EarthPosition; rtol=1e-8, atol=0.0, nans=false)
+    r = all([
+        isapprox(x.x, y.x; rtol=rtol, atol=atol, nans=nans),
+        isapprox(x.y, y.y; rtol=rtol, atol=atol, nans=nans),
+        isapprox(x.z, y.z; rtol=rtol, atol=atol, nans=nans),
+        isapprox(x.ref_point, y.ref_point; rtol=rtol, atol=atol, nans=nans),
+    ])
+    return r
 end
