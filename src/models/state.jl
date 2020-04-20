9cc076589076fda5b24ddea3289b0ab59ce8e821
asaez <alex.saez.12@gmail.com>
2020-04-20 11:58:48 +0200

    STY

--- src/models/state.jl
+++ src/models/state.jl
@@ -7,16 +7,19 @@ struct State
     angular_acceleration::Array{T, 1} where T<:Number  # body (p_dot, q_dot, r_dot) in rad/s^2
 end
 
+
 # Position getters
 get_position(state::State) = state.position
 get_llh(state::State) = get_llh(state.position)
 get_xyz_earth(state::State) = get_xyz_earth(state.position)
 get_xyz_ecef(state::State) = get_xyz_ecef(state.position)
 get_height(state::State) = get_height(state.position)
+
 # Attitude getters
 get_attitude(state::State) = state.attitude
 get_euler_angles(state::State) = get_euler_angles(state.attitude)
 get_quaternions(state::State) = get_quaternions(state.attitude)
+
 # Velocity getters
 get_body_velocity(state::State) = state.velocity
 get_horizon_velocity(state::State) = body2hor(
@@ -36,19 +39,26 @@ end
 
 # Angular velocity getters
 get_body_ang_velocity(state::State) = state.angular_velocity
-get_turn_rate(state::State) = body2hor(get_body_ang_velocity(state)...,
-                                       get_quaternions(state)...)[3]
+get_turn_rate(state::State) = body2hor(
+    get_body_ang_velocity(state)...,
+    get_quaternions(state)...,
+)[3]
 get_euler_angles_rates(state::State) = body_angular_velocity_to_euler_angles_rates(
-    get_body_ang_velocity(state)..., get_euler_angles(state)[2:3]...
+    get_body_ang_velocity(state)...,
+    get_euler_angles(state)[2:3]...,
 )
 get_quaternions_rate(state::State) = body_angular_velocity_to_quaternion_rates(
     get_body_ang_velocity(state)...,
-     get_quaternions(state)...
-     )
+    get_quaternions(state)...
+)
+
 # Acceleration getters
 get_body_accel(state::State) = state.acceleration
-get_horizon_accel(state::State) = body2hor(get_body_accel(state)...,
-                                           get_quaternions(state)...)
+get_horizon_accel(state::State) = body2hor(
+    get_body_accel(state)...,
+    get_quaternions(state)...,
+)
+
 # Angular acceleration getters
 get_body_ang_accel(state::State) = state.angular_acceleration
 
